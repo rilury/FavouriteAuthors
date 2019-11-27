e3697a48ba3859ef2d28da622624df9e06ca4875
Raluca Iordan <raiordan@deloittece.com>
2019-11-27 16:21:42 +0200

    Initial Commit

---
+++ FavouriteAuthors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FavouriteAuthors
+//
+//  Created by Iordan, Raluca on 27/11/2019.
+//  Copyright © 2019 Iordan, Raluca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
